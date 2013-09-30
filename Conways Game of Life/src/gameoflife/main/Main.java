de8cd2f84d05ea39b79b42cface896ae0b40ec5e
alexlukas <a.lukas96@gmx.de>
2013-09-30 19:58:23 +0200

    Comment added

--- Conways Game of Life/src/gameoflife/main/Main.java
+++ Conways Game of Life/src/gameoflife/main/Main.java
@@ -4,6 +4,7 @@ import gameoflife.controller.Controller;
 import gameoflife.model.GameOfLife;
 import gameoflife.view.CellFrame;
 
+//Everyone loves CGOL!
 public class Main {
 	public static void main(String[] args){
 		GameOfLife gol = new GameOfLife(30,30,true);
